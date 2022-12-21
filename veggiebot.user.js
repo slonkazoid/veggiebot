0eaac59b2fde174f696e02af2990c7134a1f4291
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-20 19:44:38 -0500

    s

--- veggiebot.user.js
+++ veggiebot.user.js
@@ -9,7 +9,6 @@
 // @updateURL    https://raw.githubusercontent.com/Vegan-PixelCanvas/veggiebot/main/veggiebot.user.js
 // @downloadURL  https://raw.githubusercontent.com/Vegan-PixelCanvas/veggiebot/main/veggiebot.user.js
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/toastify-js
 // @require      https://veggiebotserver.knobrega.com/veggieBotLibrary.js
 // ==/UserScript==
 //jshint esversion: 10
@@ -18,6 +17,15 @@
 // TO DO:
 // fix updating of incorrect pixel counters - nothing updates after first load
 // fix pixels placed counter- should update AFTER the latest pixel has been placed, lags behind 1
+
+
+//startup order
+//put up loading screen
+//global variables
+
+//fetch user
+//THEN build UI
+//THEN start bot
 
 const splash = document.createElement("div");
 (function makeLoadingScreen() { //build and display loading screen
@@ -48,26 +56,56 @@ const splash = document.createElement("div");
 //global values
 const botID = getCookie("z") ? getCookie("z") : veggieBot.randomInteger(10000, 99999); //if cookie exists, get botID from there. otherwise create new ID.
 setCookie("z", botID, 2); //save bot ID to cookie
+let user;
+let ws;
 
-//UI
-(function buildUI() {
+//load library
+
+
+//then check if user is authorized
+fetch("https://veggiebotserver.knobrega.com/user", {credentials: 'include'})
+.then((response) => {
+	if (response.status === 401) { //user is not logged in
+		window.location.replace("https://veggiebotserver.knobrega.com/auth/login");
+	}
+	else if (response.status === 200) { //user is logged in and authorized
+		response.text().then((result) => { //pull out text
+			user = JSON.parse(result); //convert text to json
+			buildUI();
+    	});
+  	}
+});
+//then build UI
+function buildUI() {
 	const div = document.createElement("div");
 	div.style = "margin-top: -42px;";
 	div.innerHTML = `
-		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 		<style>
 			.ui {
 				position: absolute;
-				display: flex;
-				flex-flow: column;
-				gap: 15px;
-				padding: 15px;
 				font-family: monospace;
+				display: flex;
+				flex-direction: column;
 
 				height: 100%;
 				background: #202123;
 				color:white;
 				font-size: 12px;
+				max-width: 340px;
+			}
+			.uiStackTop {
+				display: flex;
+				flex-flow: column;
+				gap: 15px;
+				padding: 15px;
+			}
+			.uiStackBottom {
+				display: flex;
+				flex-flow: column;
+				gap: 15px;
+				padding: 15px;
+				justify-content: flex-end;
+				flex-grow: 1;
 			}
 			.appTitle {
 				font-size: 1.7em;
@@ -139,47 +177,110 @@ setCookie("z", botID, 2); //save bot ID to cookie
 			.active {
 				background-color: #0c41a0 !important;
 			}
+			.user {
+				background: #2b2d32;
+				color:  white;
+				padding: 10px;
+				border-radius: 10px;
+				border: 2px solid #36383f;
+				
+				display: flex;
+				flex-flow: row;
+				gap: 10px;
+			}
 			a {
 				text-decoration: underline;
-				text-decoration-color: #0c41a0;
+				text-decoration-color: #1e58c0;
 				text-decoration-thickness: 2px;
+				color: 
+			}
+			.logScroller {
+				background-color: black;
+				border-radius: 10px;
+				border: 2px solid #333;
+				overflow: scroll;
+				height: 10em;
+				padding: 10px;
+				white-space: nowrap;
 			}
 		</style>
 		<div class="ui">
-			<p><span class="appTitle">VeggieBot</span><span class="appVersion">v${GM_info.script.version} · #${botID}</span></p>
-			<div class="mainStats">
-				<span class="todoCounter"></span>
-				<span class="pixelsPlaced"></span>
+			<div class="uiStackTop">
+				<p><span class="appTitle">VeggieBot</span><span class="appVersion">v${GM_info.script.version} · #${botID}</span></p>
+				<div class="mainStats">
+					<span class="todoCounter"></span>
+					<span class="pixelsPlaced"></span>
+				</div>
+				<div class="logScroller">
+				</div>
+				<table>
+					<tbody class="designsTable">
+					</tbody>
+				</table>
+				<div class="designInfo">
+					<strong class="designName"></strong>
+					<span class="designCompletion"></span>
+					<span class="designDimensions"></span>
+					<span class="designLocation"></span>
+					<span class="designSize"></span>
+					<span class="designLink"></span>
+				</div>
 			</div>
-			<table>
-				<tbody class="designsTable">
-				</tbody>
-			</table>
-			<div class="designInfo">
-				<strong class="designName"></strong>
-				<span class="designCompletion"></span>
-				<span class="designDimensions"></span>
-				<span class="designLocation"></span>
-				<span class="designSize"></span>
-				<span class="designLink"></span>
-			</div>
-			<!-- <div style="display: flex; flex-flow: row; gap: 10px;">
-				<img class="avatar" style="border-radius: 10px; width: 64px; height: 64px;";>
-				<div style="display: flex; flex-flow: column; justify-content: center;">
+			<div class="uiStackBottom">
+				<div class="user">
+				<img class="avatar" style="border-radius: 10px; width: 64px; height: 64px;" src="${`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`}">
+				<div style="display: flex; flex-flow: column; justify-content: center; gap: 1px;">
 					<span>
-						<strong class="username"></strong>
-						<span class="discriminator"></span>
+						<strong class="username">${user.username}</strong>
+						<span class="discriminator">#${user.discriminator}</span>
 					</span>
 					<br>
-					<a href="https://veggiebot.thechristmasstation.org/auth/logout" style="color: cornflowerblue; text-decoration: underline;">Log out</a>
+					<a href="https://veggiebotserver.knobrega.com/auth/logout">Log out</a>
 				</div>
-			</div> -->
+			</div>
 		</div>
 	`;
 	document.body.appendChild(div);
-})();
-function displayDesign(design) {
-	const modal = document.querySelector('.designInfo');
+}
+
+window.onload = async function startBot() { //when page is done loading, start bot
+	fetch("https://veggiebotserver.knobrega.com/designs", {credentials: 'include'}) //fetch processed designs from server
+	.then(response => response.text())
+	.then(result => {
+
+		const processedDesignArray = JSON.parse(result); //array of processed design objects
+		const designArray = []; //array of final Design objects
+
+		for (const processedDesign of processedDesignArray) { //for each processed design
+			designArray.push( new veggieBot.Design(processedDesign)); //create final Design and push to designArray
+		}
+
+		window.designArray = designArray;
+		console.log(designArray);
+
+		veggieBot.pixelTimer(designArray, pixelCallback); //start pixel placement loop
+		displayDesign(designArray[0]);
+		
+		ws = new WebSocket("wss://veggiebotserver.knobrega.com/live");
+		ws.addEventListener("open", event => {
+			ws.send(JSON.stringify({
+				type: "connect",
+				user
+			}))
+		});
+
+		splash.classList.add("hidden"); //take down loading screen
+		setTimeout(
+			() => {
+				window.location.reload();
+			},
+			(30 * 60 * 1000)
+		); //refresh page after 30 mins
+	});
+};
+
+
+function displayDesign(design) { //displays a Design in the ui's design inspector
 	document.querySelector('.designName').innerHTML = design.name;
 	document.querySelector('.designCompletion').innerHTML = `Completion: ${(design.width * design.height) - design.incorrectPixels.length} / ${design.width * design.height}`;
 	document.querySelector('.designLocation').innerHTML = `Location: <a href="https://pixelcanvas.io/@${design.xCoord},${design.yCoord}">(${design.xCoord}, ${design.yCoord})</a>`;
@@ -187,12 +288,16 @@ function displayDesign(design) {
 	document.querySelector('.designSize').innerHTML = `Size: ${design.pixels.length} pixels`;
 	document.querySelector('.designLink').innerHTML = `File: <a href="${design.url}" target="_blank" rel="noopener noreferrer">${design.url.substring(design.url.lastIndexOf('/') + 1)}</a>`;
 }
-function refreshUI(designArray) { //clears and reloads the design table in the UI
+/**
+ * Refreshes anything in the UI that changes
+ * @param {*} designArray TODO fix this
+ */
+function refreshUI(designArray) {
 	const designsTable = document.querySelector(".designsTable");
 	designsTable.innerHTML = `
-		<tr style="text-align: left;">
-			<th>Design</th>
-			<th>Pixels To Do</th>
+		<tr>
+			<th style="text-align: left;">Design</th>
+			<th style="text-align: right;">Pixels To Do</th>
 		</tr>
 	`;
 	
@@ -221,27 +326,12 @@ function refreshUI(designArray) { //clears and reloads the design table in the U
 	document.querySelector(".todoCounter").innerHTML = `Pixels to do: ${totalIncorrectPixels}`; //update pixel todo counter
 	document.querySelector(".pixelsPlaced").innerHTML = `Pixels placed: ${getCookie("pixelCounter") ? getCookie("pixelCounter") : 0}`; //update pixels placed counter
 }
-
-// //check if user is authorized
-// fetch("https://veggiebot.thechristmasstation.org/user", {credentials: 'include'})
-// .then((response) => {
-//   if (response.status === 401) { //user is not logged in
-//     window.location.replace("https://veggiebot.thechristmasstation.org/auth/login");
-//   }
-//   else if (response.status === 200) { //user is logged in and authorized
-//     response.text().then((result) => { //pull out text
-//       const user = JSON.parse(result); //convert text to json
-//
-//       //fill in the user info in the UI
-//       document.querySelector(".username").innerHTML = user.username;
-//       document.querySelector(".discriminator").innerHTML = "#" + user.discriminator;
-//       document.querySelector(".avatar").src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`;
-//     });
-//   }
-// });
-
-
-function getCookie(cname) { //returns value of cookie by name
+/**
+ * Returns value of a cookie by name
+ * @param {string} cname Name of the cookie to fetch
+ * @returns {string} Value, blank if no cookie
+ */
+function getCookie(cname) {
 	let name = cname + "=";
 	let decodedCookie = decodeURIComponent(document.cookie);
 	let ca = decodedCookie.split(';');
@@ -256,31 +346,43 @@ function getCookie(cname) { //returns value of cookie by name
 	}
 	return "";
 }
-function setCookie(cname, cvalue, exdays) { //sets cookie
+/**
+ * Sets a cookie value
+ * @param {string} cname Name of cookie
+ * @param {string} cvalue Value of cookie
+ * @param {number} exdays Days until expiration
+ */
+function setCookie(cname, cvalue, exdays) {
 	const d = new Date();
 	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
 	let expires = "expires=" + d.toUTCString();
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function pixelCallback (results) { //runs after each pixel placement attempt
+	if (results.successful === true) {
+		const oldCount = parseInt(getCookie("pixelCounter") ? getCookie("pixelCounter") : 0);
+		setCookie(
+			"pixelCounter",
+			(oldCount + 1),
+			3
+		); //update cookie
+	}
 
-window.onload = async function startBot() { //when page is done loading, start bot
-	fetch("https://veggiebotserver.knobrega.com/designs") //fetch processed designs from server
-	.then(response => response.text())
-	.then(result => {
-		const processedDesignArray = JSON.parse(result); //array of processed design objects
-		const designArray = []; //array of final Design objects
+	veggieBot.webhook(results.string);
+	// ws.send(JSON.stringify({
+	// 	type: "pixel",
+	// 	user,
+	// 	results,
+	// }))
 
-		for (const processedDesign of processedDesignArray) { //for each processed design
-			designArray.push( new veggieBot.Design(processedDesign)); //create final Design and push to designArray
-		}
+	const p = document.createElement('p')
 
-		window.designArray = designArray;
-		console.log(designArray);
+	const now = new Date();
+	const timeString = "[" + now.getHours() + ":" + now.getMinutes().toString().padStart(2, '0') + ":" + now.getSeconds().toString().padStart(2, '0') + "]";
 
-		veggieBot.pixelTimer(designArray); //start pixel placement loop
-		displayDesign(designArray[0]);
-		splash.classList.add("hidden"); //take down splash screen
-		setTimeout(function () { window.location.reload(); }, (30 * 60 * 1000)); //refresh page after 30 mins
-	});
-};
+	p.innerHTML = timeString + " " + results.string;
+	document.querySelector('.logScroller').appendChild(p)
+	refreshUI(designArray);
+	
+}
