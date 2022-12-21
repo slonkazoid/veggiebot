64dc5af2f8e29db2c9bdd3269f12af9d473d5c64
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-20 19:44:56 -0500

    s

--- veggiebot.user.js
+++ veggiebot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VeggieBot
 // @namespace    https://discord.gg/grHtzeRFAf
-// @version      3.6.1
+// @version      3.7.0
 // @description  Bot for vegan banners on pixelcanvas.io
 // @author       Vegans
 // @match        https://pixelcanvas.io/*
