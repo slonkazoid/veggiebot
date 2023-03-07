250699411b1e50cb2f3689338be304127dfdf78d
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-03-07 10:10:33 -0500

    test

--- veggiebot.user.js
+++ veggiebot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VeggieBot
-// @version      5.0.0
+// @version      5.0.2
 // @author       Vegans
 // @match        https://pixelcanvas.io/*
 // @icon         https://pixelcanvas.io/favicon.ico
