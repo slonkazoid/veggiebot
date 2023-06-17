e240ae3ebbf4537e0fecd7de9426c0b0b8cb0966
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-06-17 01:52:48 -0400

    jank

--- veggiebot.user.js
+++ veggiebot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VeggieBot
-// @version      5.1.0
+// @version      5.2.0
 // @author       Vegans
 // @match        https://pixelcanvas.io/*
 // @icon         https://pixelcanvas.io/favicon.ico
@@ -9,21 +9,17 @@
 // @grant        none
 // ==/UserScript==
 
+//refresh page after one hour
 setTimeout(() => {
 	window.location.reload();
-}, 60 * 60 * 1000); //refresh page after 1 hour
+}, 60 * 60 * 1000);
 
+//base URL
 const baseURL =
 	getCookie("dev") === "true"
-		? "https://veggiebotserver-dev.knobrega.com"
+		? "https://veggiebot-dev.knobrega.com"
 		: "https://veggiebotserver.knobrega.com";
 window.baseURL = baseURL;
-
-// load the library
-//TODO load dynamically. wait for library to load and handle error if it doesn't load.
-const library = document.createElement("script");
-library.src = `${baseURL}/veggieBotLibrary.js`;
-document.body.appendChild(library);
 
 //global values
 const botID =
@@ -32,221 +28,226 @@ const version = GM_info.script.version;
 setCookie("z", botID, 30); //save bot ID to cookie
 let user;
 
-//check if user is authorized
-fetch(baseURL + "/auth/user", {
-	credentials: "include",
-}).then((response) => {
-	if (response.status === 401) {
-		//user is not logged in
-		window.location.replace(baseURL + "/auth/login");
-		return;
-	}
+(async () => {
+	// load the library
+	await loadScript(`${baseURL}/veggieBotLibrary.js`);
 
-	if (response.status === 200) {
-		//user is logged in and authorized
-		response.text().then((result) => {
-			//pull out text
-			user = JSON.parse(result); //convert text to json
-			buildUI();
-		});
-		return;
-	}
-
-	alert("unexpected response recieved from veggiebotserver user endpoint");
-});
-
-//then build UI
-function buildUI() {
-	//load fontawesome
-	const iconScript = document.createElement("script");
-	iconScript.src = "https://kit.fontawesome.com/5f09e108f6.js";
-	iconScript.crossOrigin = "anonymous";
-	document.body.appendChild(iconScript);
-
-	const div = document.createElement("div");
-	div.classList.add("veggieBot");
-
-	div.innerHTML = /*html*/ `
-		
-		<link rel="preconnect" href="https://fonts.googleapis.com" />
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-		<link
-			href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Lexend+Deca&family=Nunito&display=swap"
-			rel="stylesheet"
-		/>
-		<style>
-			.veggieBot {
-				font-family: "Nunito", sans-serif;
-				color: white;
-				font-size: 0.75rem;
-			}
-			.sidebar {
-				position: absolute;
-				height: 100%;
-				width: 86px;
-				overflow: scroll;
-
-				display: flex;
-				flex-direction: column;
-
-				background: #202123;
-			}
-			.uiTop {
-				background-color: ${getCookie("dev") === "true" ? "#ffe300" : "#0c41a0"};
-				color: ${getCookie("dev") === "true" ? "black" : "white"};
-				font-family: "Lexend Deca", sans-serif;
-				
-				padding: 15px;
-				border-bottom: 2px solid #3968bd;
-			}
-			.uiStackTop {
-				display: flex;
-				flex-flow: column;
-				gap: 15px;
-				padding: 15px;
-				height: 100%;
-			}
-			.card {
-				background: #2b2d32;
-				color:  white;
-				padding: 10px;
-				border-radius: 10px;
-				border: 2px solid #36383f;
-
-				display: flex;
-				flex-flow: column;
-				gap: 10px;
-			}
-			.user {
-				background: #2b2d32;
-				color:  white;
-				padding: 15px;
-				border-top: 2px solid #36383f;
-				
-				display: flex;
-				flex-flow: row;
-				gap: 10px;
-			}
-			.logScroller {
-				background-color: black;
-				border-radius: 10px;
-				border: 2px solid #333;
-				overflow: scroll;
-				padding: 10px;
-				white-space: nowrap;
-				color: white;
-				font-family: monospace;
-			}
-			#gameWindow + div + div {
-				width: 100px;
-			}
-			.textInput {
-				background-color: #2b2d32;
-				color: white;
-				border-radius: 4px;
-				border: 2px solid #36383f;
-				min-width: 0;
-				padding: 3px 7px;
-			}
-			.navIcon {
-				font-size: 2rem;
-				display: block;
-			}
-			dialog {
-				background: none;
-			}
-			dialog::backdrop {
-				background-color: #000a;
-			}
-
-		</style>
-
-		<dialog class="jumpTool">
-			<div class="card">
-				<div style="display: flex; flex-direction: column; gap: 5px;">
-					<strong>Jump to location</strong>
-					<form id="jumpForm">
-						<div style="display: flex; flex-direction: column; gap: 8px;">
-							<input class="textInput jumpX" placeholder="x coord" required>
-							<input class="textInput jumpY" placeholder="y coord" required>
-							<input type="submit" value="Go" style="background-color: #0e4ec1; padding: 5px 10px; border-radius: 4px; cursor: pointer;">
-						</div>
-					</form>
-				</div>
-			</div>
-		</dialog>
-
-		<dialog class="userModal">
-			<div class="card">
-				<img style="border-radius: 10px; width: 40px; height: 40px;" src="${
-					user.avatar
-						? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`
-						: "https://cdn.discordapp.com/embed/avatars/0.png"
-				}">
-				<a href="${baseURL}/auth/logout">Log out</a>
-				<span class="appVersion">v${version} · #${botID}</span>
-			</div>
-		</dialog>
-
-		<dialog class="log logScroller">
-		</dialog>
-
-		<div class="sidebar">
-			<div class="uiTop">
-				<img src="https://veggiebotserver.knobrega.com/logo-primary.png">
-			</div>
-			<div class="uiStackTop">
-				<div class="card logIcon" style="cursor: pointer;">
-					<i class="fa-solid fa-list-ul navIcon"></i>
-				</div>
-				<div class="card jumpToolIcon" style="cursor: pointer;">
-					<i class="fa-solid fa-compass navIcon"></i>
-				</div>
-				<a href="${baseURL}">
-					<div class="card">
-						<i class="fa-solid fa-chart-column navIcon"></i>
-					</div>
-				</a>
-			</div>
-			<span style="transform: rotate(270deg); font-family: 'Lexend Deca'; font-size: 1.7rem; margin-bottom: 50px; white-space: nowrap;"><span class="pixelsPlaced">0</span> pixels placed</span>
-			<div class="user">
-				<img class="avatar" style="border-radius: 10px" src="${
-					user.avatar
-						? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`
-						: "https://cdn.discordapp.com/embed/avatars/0.png"
-				}">
-			</div>
-		</div>
-	`;
-	document.body.appendChild(div);
-
-	//jump
-	jumpToolForm = document.querySelector("#jumpForm");
-	jumpToolForm.addEventListener("submit", (e) => {
-		e.preventDefault();
-		window.setView(
-			parseInt(document.querySelector(".jumpX").value),
-			parseInt(document.querySelector(".jumpY").value)
-		);
-		document.querySelector(".jumpX").value = null;
-		document.querySelector(".jumpY").value = null;
-		document.querySelector(".jumpTool").close();
+	//check if user is authorized
+	const response = await fetch(baseURL + "/auth/user", {
+		credentials: "include",
 	});
 
-	document.querySelector(".jumpToolIcon").onclick = () => {
-		document.querySelector(".jumpTool").showModal();
-	};
+	await a();
+	async function a() {
+		if (response.status === 401) {
+			//user is not logged in
+			window.location.replace(baseURL + "/auth/login");
+			return;
+		}
+		if (response.status === 200) {
+			//user is logged in and authorized
+			user = await response.json();
+			return;
+		}
 
-	document.querySelector(".logIcon").onclick = () => {
-		document.querySelector(".log").showModal();
-	};
+		alert("unexpected response recieved from veggiebotserver user endpoint");
+	}
 
-	document.querySelector(".avatar").onclick = () => {
-		document.querySelector(".userModal").showModal();
-	};
-}
+	//then build UI
+	function buildUI() {
+		//load fontawesome
+		const iconScript = document.createElement("script");
+		iconScript.src = "https://kit.fontawesome.com/5f09e108f6.js";
+		iconScript.crossOrigin = "anonymous";
+		document.body.appendChild(iconScript);
+
+		const div = document.createElement("div");
+		div.classList.add("veggieBot");
+
+		div.innerHTML = /*html*/ `
+			
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Chivo+Mono&family=Lexend+Deca&family=Nunito&display=swap"
+				rel="stylesheet"
+			/>
+			<style>
+				.veggieBot {
+					font-family: "Nunito", sans-serif;
+					color: white;
+					font-size: 0.75rem;
+				}
+				.sidebar {
+					position: absolute;
+					height: 100%;
+					width: 86px;
+					overflow: scroll;
+	
+					display: flex;
+					flex-direction: column;
+	
+					background: #202123;
+				}
+				.uiTop {
+					background-color: ${getCookie("dev") === "true" ? "#ffe300" : "#0c41a0"};
+					color: ${getCookie("dev") === "true" ? "black" : "white"};
+					font-family: "Lexend Deca", sans-serif;
+					
+					padding: 15px;
+					border-bottom: 2px solid #3968bd;
+				}
+				.uiStackTop {
+					display: flex;
+					flex-flow: column;
+					gap: 15px;
+					padding: 15px;
+					height: 100%;
+				}
+				.card {
+					background: #2b2d32;
+					color:  white;
+					padding: 10px;
+					border-radius: 10px;
+					border: 2px solid #36383f;
+	
+					display: flex;
+					flex-flow: column;
+					gap: 10px;
+				}
+				.user {
+					background: #2b2d32;
+					color:  white;
+					padding: 15px;
+					border-top: 2px solid #36383f;
+					
+					display: flex;
+					flex-flow: row;
+					gap: 10px;
+				}
+				.logScroller {
+					background-color: black;
+					border-radius: 10px;
+					border: 2px solid #333;
+					overflow: scroll;
+					padding: 10px;
+					white-space: nowrap;
+					color: white;
+					font-family: monospace;
+				}
+				#gameWindow + div + div {
+					width: 100px;
+				}
+				.textInput {
+					background-color: #2b2d32;
+					color: white;
+					border-radius: 4px;
+					border: 2px solid #36383f;
+					min-width: 0;
+					padding: 3px 7px;
+				}
+				.navIcon {
+					font-size: 2rem;
+					display: block;
+				}
+				dialog {
+					background: none;
+				}
+				dialog::backdrop {
+					background-color: #000a;
+				}
+	
+			</style>
+	
+			<dialog class="jumpTool">
+				<div class="card">
+					<div style="display: flex; flex-direction: column; gap: 5px;">
+						<strong>Jump to location</strong>
+						<form id="jumpForm">
+							<div style="display: flex; flex-direction: column; gap: 8px;">
+								<input class="textInput jumpX" placeholder="x coord" required>
+								<input class="textInput jumpY" placeholder="y coord" required>
+								<input type="submit" value="Go" style="background-color: #0e4ec1; padding: 5px 10px; border-radius: 4px; cursor: pointer;">
+							</div>
+						</form>
+					</div>
+				</div>
+			</dialog>
+	
+			<dialog class="userModal">
+				<div class="card">
+					<img style="border-radius: 10px; width: 40px; height: 40px;" src="${
+						user.avatar
+							? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`
+							: "https://cdn.discordapp.com/embed/avatars/0.png"
+					}">
+					<a href="${baseURL}/auth/logout">Log out</a>
+					<span class="appVersion">v${version} · #${botID}</span>
+				</div>
+			</dialog>
+	
+			<dialog class="log logScroller">
+			</dialog>
+	
+			<div class="sidebar">
+				<div class="uiTop">
+					<img src="https://veggiebotserver.knobrega.com/logo-primary.png">
+				</div>
+				<div class="uiStackTop">
+					<div class="card logIcon" style="cursor: pointer;">
+						<i class="fa-solid fa-list-ul navIcon"></i>
+					</div>
+					<div class="card jumpToolIcon" style="cursor: pointer;">
+						<i class="fa-solid fa-compass navIcon"></i>
+					</div>
+					<a href="${baseURL}">
+						<div class="card">
+							<i class="fa-solid fa-chart-column navIcon"></i>
+						</div>
+					</a>
+				</div>
+				<span style="transform: rotate(270deg); font-family: 'Lexend Deca'; font-size: 1.7rem; margin-bottom: 50px; white-space: nowrap;"><span class="pixelsPlaced">0</span> pixels placed</span>
+				<div class="user">
+					<img class="avatar" style="border-radius: 10px" src="${
+						user.avatar
+							? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp?size=64`
+							: "https://cdn.discordapp.com/embed/avatars/0.png"
+					}">
+				</div>
+			</div>
+		`;
+		document.body.appendChild(div);
+
+		//jump
+		jumpToolForm = document.querySelector("#jumpForm");
+		jumpToolForm.addEventListener("submit", (e) => {
+			e.preventDefault();
+			window.setView(
+				parseInt(document.querySelector(".jumpX").value),
+				parseInt(document.querySelector(".jumpY").value)
+			);
+			document.querySelector(".jumpX").value = null;
+			document.querySelector(".jumpY").value = null;
+			document.querySelector(".jumpTool").close();
+		});
+
+		document.querySelector(".jumpToolIcon").onclick = () => {
+			document.querySelector(".jumpTool").showModal();
+		};
+
+		document.querySelector(".logIcon").onclick = () => {
+			document.querySelector(".log").showModal();
+		};
+
+		document.querySelector(".avatar").onclick = () => {
+			document.querySelector(".userModal").showModal();
+		};
+	}
+	buildUI();
+})();
 
 window.onload = async () => {
+	console.log(typeof veggieBot);
 	veggieBot.pixelTimer(pixelCallback); //start pixel placement loop
 };
 
@@ -324,3 +325,23 @@ window.toggleDev = () => {
 	setCookie("dev", getCookie("dev") === "true" ? "false" : "true", 10);
 	window.location.reload();
 };
+
+function loadScript(url) {
+	return new Promise((resolve, reject) => {
+		try {
+			const script = document.createElement("script");
+			script.src = url;
+
+			script.addEventListener("load", (ev) => {
+				resolve({ status: true });
+			});
+			script.addEventListener("error", (ev) => {
+				reject({ status: false, message: `Failed to load script ${url}` });
+			});
+
+			document.body.appendChild(script);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
