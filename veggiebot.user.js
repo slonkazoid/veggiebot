3d57ce37e73e22de446012c07e98f490966b494e
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-06-26 18:30:09 -0400

    a

--- veggiebot.user.js
+++ veggiebot.user.js
@@ -1,60 +1,64 @@
 // ==UserScript==
 // @name         VeggieBot
 // @version      5.4.2
-// @match        https://pixelcanvas.io/*
+// @include      https://pixelcanvas.io/*
+// @include	     https://veggiepixel.org*
+// @include		 https://dev.veggiepixel.org*
 // @icon         https://pixelcanvas.io/favicon.ico
 // @updateURL    https://github.com/Vegan-PixelCanvas/veggiebot/raw/main/veggiebot.user.js
 // @downloadURL  https://github.com/Vegan-PixelCanvas/veggiebot/raw/main/veggiebot.user.js
 // @grant        none
 // ==/UserScript==
 
-//refresh page after one hour
-setTimeout(() => {
-	window.location.reload();
-}, 60 * 60 * 1000);
+window.userscript = GM_info.script.version;
+if (window.location.host === "pixelcanvas.io") {
+	//refresh page after one hour
+	setTimeout(() => {
+		window.location.reload();
+	}, 60 * 60 * 1000);
 
-//base URL
-const baseURL =
-	getCookie("dev") === "true"
-		? "https://dev.veggiepixel.org"
-		: "https://veggiepixel.org";
-window.baseURL = baseURL;
+	//base URL
+	const baseURL =
+		getCookie("dev") === "true"
+			? "https://dev.veggiepixel.org"
+			: "https://veggiepixel.org";
+	window.baseURL = baseURL;
 
-//global values
-const botID =
-	getCookie("z") || Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000; //if cookie exists, get botID from there. otherwise create new ID.
-const version = GM_info.script.version;
-setCookie("z", botID, 30); //save bot ID to cookie
-let user;
+	//global values
+	const botID =
+		getCookie("z") || Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000; //if cookie exists, get botID from there. otherwise create new ID.
+	const version = GM_info.script.version;
+	setCookie("z", botID, 30); //save bot ID to cookie
+	let user;
 
-window.onload = async () => {
-	//check if user is authorized
-	const response = await fetch(baseURL + "/auth/user", {
-		credentials: "include",
-	});
+	window.onload = async () => {
+		//check if user is authorized
+		const response = await fetch(baseURL + "/auth/user", {
+			credentials: "include",
+		});
 
-	if (response.status === 401) {
-		//user is not logged in
-		window.location.replace(baseURL + "/auth/login");
-	} else if (response.status === 200) {
-		//user is logged in and authorized
-		user = await response.json();
-	} else {
-		alert("unexpected response recieved from veggiebotserver user endpoint");
-	}
+		if (response.status === 401) {
+			//user is not logged in
+			window.location.replace(baseURL + "/auth/login");
+		} else if (response.status === 200) {
+			//user is logged in and authorized
+			user = await response.json();
+		} else {
+			alert("unexpected response recieved from veggiebotserver user endpoint");
+		}
 
-	// load the library
-	await loadScript(`${baseURL}/veggieBotLibrary.js`);
+		// load the library
+		await loadScript(`${baseURL}/veggieBotLibrary.js`);
 
-	//then build UI
-	function buildUI() {
-		//load icons
-		loadScript("https://kit.fontawesome.com/5f09e108f6.js");
+		//then build UI
+		function buildUI() {
+			//load icons
+			loadScript("https://kit.fontawesome.com/5f09e108f6.js");
 
-		const div = document.createElement("div");
-		div.classList.add("veggieBot");
+			const div = document.createElement("div");
+			div.classList.add("veggieBot");
 
-		div.innerHTML = /*html*/ `
+			div.innerHTML = /*html*/ `
 				
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -212,130 +216,140 @@ window.onload = async () => {
 					
 				</dialog>
 			`;
-		document.body.appendChild(div);
+			document.body.appendChild(div);
 
-		//jump
-		jumpToolForm = document.querySelector("#jumpForm");
-		jumpToolForm.addEventListener("submit", (e) => {
-			e.preventDefault();
-			window.setView(
-				parseInt(document.querySelector(".jumpX").value),
-				parseInt(document.querySelector(".jumpY").value)
-			);
-			document.querySelector(".jumpX").value = null;
-			document.querySelector(".jumpY").value = null;
-			document.querySelector(".jumpTool").close();
+			//jump
+			jumpToolForm = document.querySelector("#jumpForm");
+			jumpToolForm.addEventListener("submit", (e) => {
+				e.preventDefault();
+				window.setView(
+					parseInt(document.querySelector(".jumpX").value),
+					parseInt(document.querySelector(".jumpY").value)
+				);
+				document.querySelector(".jumpX").value = null;
+				document.querySelector(".jumpY").value = null;
+				document.querySelector(".jumpTool").close();
+			});
+
+			document.querySelector(".jumpToolIcon").onclick = () => {
+				document.querySelector(".jumpTool").showModal();
+			};
+
+			document.querySelector(".logIcon").onclick = () => {
+				document.querySelector(".log").showModal();
+			};
+
+			document.querySelector(".avatar").onclick = () => {
+				document.querySelector(".userModal").showModal();
+			};
+		}
+		buildUI();
+
+		if (!window.bundleJS) alert("ERROR: Failed to load bundle.js overide");
+		console.log("Bundle override", window.bundleJS);
+
+		veggieBot.pixelTimer(pixelCallback); //start pixel placement loop
+	};
+
+	function refreshUI() {
+		const pixelsPlaced = document.querySelector(".pixelsPlaced");
+		pixelsPlaced.innerHTML = Intl.NumberFormat().format(
+			getCookie("pixelCounter") || 0
+		);
+	}
+	/**
+	 * Returns value of a cookie by name
+	 * @param {string} cname Name of the cookie to fetch
+	 * @returns {string} Value, blank if no cookie
+	 */
+	function getCookie(cname) {
+		let name = cname + "=";
+		let decodedCookie = decodeURIComponent(document.cookie);
+		let ca = decodedCookie.split(";");
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i];
+			while (c.charAt(0) == " ") {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return null;
+	}
+	/**
+	 * Sets a cookie value
+	 * @param {string} cname Name of cookie
+	 * @param {string} cvalue Value of cookie
+	 * @param {number} exdays Days until expiration
+	 */
+	function setCookie(cname, cvalue, exdays) {
+		const d = new Date();
+		d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+		let expires = "expires=" + d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+	function pixelCallback(results) {
+		//runs after each pixel placement attempt
+
+		if (results.successful === true) {
+			const oldCount = parseInt(getCookie("pixelCounter") || 0);
+			setCookie("pixelCounter", oldCount + 1, 3);
+		}
+
+		window.setWait(results.waitMS);
+
+		veggieBot.webhook(
+			results.string,
+			user,
+			version,
+			botID,
+			!results.successful
+		);
+
+		const p = document.createElement("p");
+
+		const now = new Date();
+		const timeString =
+			"[" +
+			now.getHours() +
+			":" +
+			now.getMinutes().toString().padStart(2, "0") +
+			":" +
+			now.getSeconds().toString().padStart(2, "0") +
+			"]";
+
+		p.innerHTML = timeString + " " + results.string;
+		const logScroller = document.querySelector(".logScroller");
+		logScroller.appendChild(p);
+		logScroller.scrollTop = logScroller.scrollHeight;
+
+		refreshUI();
+	}
+
+	window.toggleDev = () => {
+		setCookie("dev", getCookie("dev") === "true" ? "false" : "true", 10);
+		window.location.reload();
+	};
+
+	function loadScript(url) {
+		return new Promise((resolve, reject) => {
+			try {
+				const script = document.createElement("script");
+				script.src = url;
+
+				script.addEventListener("load", (ev) => {
+					resolve({ status: true });
+				});
+				script.addEventListener("error", (ev) => {
+					reject({ status: false, message: `Failed to load script ${url}` });
+				});
+
+				document.body.appendChild(script);
+			} catch (error) {
+				reject(error);
+			}
 		});
-
-		document.querySelector(".jumpToolIcon").onclick = () => {
-			document.querySelector(".jumpTool").showModal();
-		};
-
-		document.querySelector(".logIcon").onclick = () => {
-			document.querySelector(".log").showModal();
-		};
-
-		document.querySelector(".avatar").onclick = () => {
-			document.querySelector(".userModal").showModal();
-		};
 	}
-	buildUI();
-
-	veggieBot.pixelTimer(pixelCallback); //start pixel placement loop
-};
-
-function refreshUI() {
-	const pixelsPlaced = document.querySelector(".pixelsPlaced");
-	pixelsPlaced.innerHTML = Intl.NumberFormat().format(
-		getCookie("pixelCounter") || 0
-	);
-}
-/**
- * Returns value of a cookie by name
- * @param {string} cname Name of the cookie to fetch
- * @returns {string} Value, blank if no cookie
- */
-function getCookie(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(";");
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == " ") {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return null;
-}
-/**
- * Sets a cookie value
- * @param {string} cname Name of cookie
- * @param {string} cvalue Value of cookie
- * @param {number} exdays Days until expiration
- */
-function setCookie(cname, cvalue, exdays) {
-	const d = new Date();
-	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-	let expires = "expires=" + d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function pixelCallback(results) {
-	//runs after each pixel placement attempt
-
-	if (results.successful === true) {
-		const oldCount = parseInt(getCookie("pixelCounter") || 0);
-		setCookie("pixelCounter", oldCount + 1, 3);
-	}
-
-	window.setWait(results.waitMS);
-
-	veggieBot.webhook(results.string, user, version, botID, !results.successful);
-
-	const p = document.createElement("p");
-
-	const now = new Date();
-	const timeString =
-		"[" +
-		now.getHours() +
-		":" +
-		now.getMinutes().toString().padStart(2, "0") +
-		":" +
-		now.getSeconds().toString().padStart(2, "0") +
-		"]";
-
-	p.innerHTML = timeString + " " + results.string;
-	const logScroller = document.querySelector(".logScroller");
-	logScroller.appendChild(p);
-	logScroller.scrollTop = logScroller.scrollHeight;
-
-	refreshUI();
-}
-
-window.toggleDev = () => {
-	setCookie("dev", getCookie("dev") === "true" ? "false" : "true", 10);
-	window.location.reload();
-};
-
-function loadScript(url) {
-	return new Promise((resolve, reject) => {
-		try {
-			const script = document.createElement("script");
-			script.src = url;
-
-			script.addEventListener("load", (ev) => {
-				resolve({ status: true });
-			});
-			script.addEventListener("error", (ev) => {
-				reject({ status: false, message: `Failed to load script ${url}` });
-			});
-
-			document.body.appendChild(script);
-		} catch (error) {
-			reject(error);
-		}
-	});
 }
